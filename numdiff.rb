f1e283b7cbdee63c21b93508e2c9b193c040c1c3
Denis Davydov <davydden@gmail.com>
2015-10-06 08:35:17 +0200

    Numdiff: update url

--- numdiff.rb
+++ numdiff.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Numdiff < Formula
   homepage "http://www.nongnu.org/numdiff"
-  url "http://mirror2.klaus-uwe.me/nongnu/numdiff/numdiff-5.8.1.tar.gz"
+  url "http://mirror.unicorncloud.org/savannah-nongnu//numdiff/numdiff-5.8.1.tar.gz"
   sha1 "dae1fac5d2ba2e40b7713d6ef9647c94b572b4e1"
 
   # we need libintl.h which is normally a part of libc6-dev
